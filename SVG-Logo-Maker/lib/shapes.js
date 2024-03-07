aaea3afe372c0b48011925f217743c8c56465df5
Vineysha Olivo <vineyshaolivo@gmail.com>
2024-03-07 14:47:23 -0500

    Added shapes.js file

---
+++ SVG-Logo-Maker/lib/shapes.js
@@ -0,0 +1,37 @@
+// Defines what is a shape
+class Shape {
+  constructor() {
+    this.color = "";
+  }
+ 
+  setColor(colorVar) {
+    this.color = colorVar;
+  }
+}
+
+// Triangle class
+class Triangle extends Shape {
+  render() {
+    
+    return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
+  }
+}
+
+// Square class 
+class Square extends Shape {
+  render() {
+   
+    return `<rect x="73" y="40" width="160" height="160" fill="${this.color}" />`;
+  }
+}
+
+// Circle class 
+class Circle extends Shape {
+  render() {
+    
+    return `<circle cx="150" cy="115" r="80" fill="${this.color}" />`;
+  }
+}
+
+// Exports
+module.exports = { Triangle, Square, Circle };
